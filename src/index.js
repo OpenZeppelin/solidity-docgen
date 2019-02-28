8865438a6e57998fc57d5abcd26e737d776aa20a
Francisco Giordano <frangio.1@gmail.com>
2019-02-28 15:35:54 -0500

    add glue functions

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+import path from 'path';
+import fs from 'fs';
+import mkdirp from 'mkdirp';
+import { promisify } from 'util';
+import { gatherDocs } from './gather';
+import { renderDocs } from './render';
+
+const mkdirpAsync = promisify(mkdirp);
+const writeFileAsync = promisify(fs.writeFile);
+
+export async function renderDirectoryDocs(directory) {
+  const docs = await gatherDocs(directory);
+  return _.mapValues(docs, renderDocs);
+}
+
+export async function writeDocs(docs, outputDirectory) {
+  await Promise.all(Object.keys(docs).map(async function (name) {
+    const outputFile = path.format({
+      dir: outputDirectory,
+      ext: '.md',
+      name,
+    });
+    await mkdirpAsync(path.dirname(outputFile));
+    await writeFileAsync(outputFile, docs[name]);
+  }));
+}

b5971f002ec0b7fe13ba5c68b1ab55577c4a7839
Francisco Giordano <frangio.1@gmail.com>
2019-06-25 01:28:39 -0300

    simplify Page test

--- src/pages.test.ts
+++ src/pages.test.ts
@@ -1,7 +1,5 @@
 import test from 'ava';
 
-import Handlebars from 'handlebars';
-
 import { Page } from './pages';
 import { SoliditySource } from './sources/solidity';
 import { SolcOutputBuilder } from './sources/solc';
@@ -16,7 +14,7 @@ test('single file no contracts', t => {
   t.is(page.contracts.length, 0);
 });
 
-test('single file multiple contracts ', t => {
+test('single file multiple contracts', t => {
   const source = buildSoliditySource(b => b
     .file('Foo.sol')
       .contract('Foo')
@@ -44,77 +42,35 @@ test('filter subdirectory ', t => {
   t.is(page.contracts[0].name, 'Bar');
 });
 
-test('render nested subdirectories', t => {
+test('filter nested subdirectories', t => {
   const source = buildSoliditySource(b => b
     .file('sub/Bar.sol')
       .contract('Bar')
     .file('sub/sub2/Foo.sol')
       .contract('Foo')
   );
-  const template = ({ contracts }) => contracts.map(({ name }) => name);
 
   const page = new Page('sub', {}, '', source);
-  const rendered = template(page);
 
-  t.is(rendered.length, 2);
-  t.assert(rendered.includes('Bar'));
-  t.assert(rendered.includes('Foo'));
+  t.is(page.contracts.length, 2);
+  t.assert(page.contracts.some(c => c.name === 'Bar'));
+  t.assert(page.contracts.some(c => c.name === 'Foo'));
 });
 
-test('handlebars', t => {
-  const source = buildSoliditySource(b => b
-    .file('Foo.sol')
-      .contract('Foo')
-      .contract('Bar')
-  );
-  const template = Handlebars.compile(`\
-{{#contracts}}
-{{name}}
-{{/contracts}}`);
-
-  const page = new Page('', {}, '', source);
-  const rendered = template(page);
-
-  t.is(rendered, `Foo\nBar\n`);
-});
-
-test('handlebars with functions', t => {
-  const source = buildSoliditySource(b => b
-    .file('Foo.sol')
-      .contract('Foo')
-        .function('test1')
-        .function('test2')
-  );
-  const template = Handlebars.compile(`\
-{{#contracts}}
-{{name}}
-{{#functions}}
-{{name}}
-{{/functions}}
-{{/contracts}}`);
-
-  const page = new Page('', {}, '', source);
-  const rendered = template(page);
-
-  t.is(rendered, `Foo\ntest1\ntest2\n`);
-});
-
-test('page with intro', t => {
-  const source = buildSoliditySource(b => b);
-  const template = ({ intro }) => intro;
+test('intro', t => {
+  const source = buildSoliditySource();
 
   const page = new Page('', {}, 'intro', source);
-  const rendered = template(page);
 
-  t.is(rendered, `intro`);
+  t.is(page.intro, `intro`);
 });
 
 test('frontmatter', t => {
-  const source = buildSoliditySource(b => b);
+  const source = buildSoliditySource();
   const frontmatterData = { a: 1 };
   const page = new Page('', frontmatterData, '', source);
 
-  t.is(page.frontmatter, 'a: 1\n');
+  t.is(page.frontmatter, '---\na: 1\n---');
 });
 
 function buildSoliditySource(builder?: (b: SolcOutputBuilder) => void): SoliditySource {

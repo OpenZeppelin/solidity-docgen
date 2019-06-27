40a729bde5a9d8a1cedd67209369bd79c2d9ef3f
Francisco Giordano <frangio.1@gmail.com>
2019-06-27 18:16:17 -0300

    replace type assertion with non-null assertion

--- src/docgen.ts
+++ src/docgen.ts
@@ -56,8 +56,8 @@ export async function docgen(options: Options) {
 }
 
 function parsePage(contents: string): { frontmatterData: {}, intro: string } {
-  const match = <RegExpMatchArray> // this regexp will always match
-    contents.match(/^(?:---\n([^]*?\n)---\n)?([^]*)/);
+  const match = // non-null assertion because this regexp always matches
+    contents.match(/^(?:---\n([^]*?\n)?---\n)?([^]*)$/)!;
   const frontmatterData = match[1] ? yaml.safeLoad(match[1]) : {};
   const intro = match[2];
   return { frontmatterData, intro };

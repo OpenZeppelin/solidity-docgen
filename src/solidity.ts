32f929c706c261488a1a8d1d65ae09675d2c5b00
Francisco Giordano <frangio.1@gmail.com>
2019-07-21 20:52:29 -0300

    add modifiers to linkable items list

--- src/solidity.ts
+++ src/solidity.ts
@@ -90,7 +90,7 @@ export class SolidityContract implements Linkable {
   }
 
   get linkable(): Linkable[] {
-    return [this, ...this.functions, ...this.events];
+    return [this, ...this.modifiers, ...this.functions, ...this.events];
   }
 
   get inheritance(): SolidityContract[] {

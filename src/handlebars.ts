ffe4c9d8c90779b55683799cf9e9a31e429313ac
Sébastien Krafft <skrafft@gmail.com>
2019-08-06 16:59:36 +0200

    Handlebar linebreaks function to enhance readability of multiple @dev comments

--- src/handlebars.ts
+++ src/handlebars.ts
@@ -2,6 +2,7 @@ import handlebars from 'handlebars';
 
 const H = handlebars.create();
 H.registerHelper('slug', slug);
+H.registerHelper('linebreaks', linebreaks);
 
 export type Template<Context> = (context: Context) => string;
 
@@ -11,4 +12,9 @@ export function compile(template: string): Template<unknown> {
 
 export function slug(str: string): string {
   return str.replace(/\W/g, '-');
+}
+
+export function linebreaks(str: string): string {
+  if (!str) return '';
+  return str.replace(/\n/g, "  \n");
 }

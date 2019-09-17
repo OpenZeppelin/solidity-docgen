6a44527f047931e72e81676c13995f75f4ef19c7
barakman <barakman@yahoo.com>
2019-09-18 01:07:22 +0300

    Allow more advanced programming via the template file (#46)
    * Allow more advanced programming via the template file
    
    This is a feature-request for allowing users to apply more advanced configuration via the input template (HBS) file.
    
    Personal motivation:
    I would like to include only `public` and `external` functions in the output documentation.
    While the `solidity-docgen` package excludes `private` functions from the output, it still includes `internal` functions.
    
    At present, only a handful of basic operators are supported by the HandleBars language syntax (`if`, `else`, `unless` and a few more - see https://handlebarsjs.com/builtin_helpers.html).
    
    Thus, neither string comparison nor conditional statements are supported (while both are required in order to compare the function `visibility` string to `"public"` and `"external"`).
    
    This short extension will allow the above, via (for example):
    ```
    # Functions:
    {{#ownFunctions}}
    {{#if (or (eq visibility "public") (eq visibility "external"))}}
    - [`{{name}}({{args}})`]
    {{/if}}
    {{/ownFunctions}}
    ```
    Thanks
    
    * Update src/handlebars.ts
    
    Co-Authored-By: Francisco Giordano <frangio.1@gmail.com>
    
    * Update src/handlebars.ts
    
    Co-Authored-By: Francisco Giordano <frangio.1@gmail.com>
    
    * trigger ci

--- src/handlebars.ts
+++ src/handlebars.ts
@@ -2,6 +2,32 @@ import handlebars from 'handlebars';
 
 const H = handlebars.create();
 H.registerHelper('slug', slug);
+H.registerHelper({
+    eq: function (v1, v2) {
+        return v1 === v2;
+    },
+    ne: function (v1, v2) {
+        return v1 !== v2;
+    },
+    lt: function (v1, v2) {
+        return v1 < v2;
+    },
+    gt: function (v1, v2) {
+        return v1 > v2;
+    },
+    lte: function (v1, v2) {
+        return v1 <= v2;
+    },
+    gte: function (v1, v2) {
+        return v1 >= v2;
+    },
+    and: function () {
+        return [...arguments].every(Boolean);
+    },
+    or: function () {
+        return [...arguments].some(Boolean);
+    }
+});
 
 export type Template<Context> = (context: Context) => string;
 

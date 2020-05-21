66735e36c985d10928fb2bac7724c737523e9ad5
Francisco Giordano <frangio.1@gmail.com>
2020-05-21 13:24:22 -0300

    fix cli tests setup

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -4,10 +4,6 @@ import { promises as fs } from 'fs';
 import { Docgen } from './cli';
 
 test('fixture 001', async t => {
-  for (const e of await fs.readdir('fixtures/001/output')) {
-    await fs.unlink(`fixtures/001/output/${e}`);
-  }
-  await fs.rmdir('fixtures/001/output');
   await Docgen.run([
     '--input', 'fixtures/001/input',
     '--output', 'fixtures/001/output',
@@ -16,3 +12,18 @@ test('fixture 001', async t => {
   const output = await fs.readFile('fixtures/001/output/index.md', 'utf8');
   t.snapshot(output);
 });
+
+async function cleanFixtureOutput(num: string) {
+  const outputPath = `fixtures/${num}/output`;
+
+  try {
+    await fs.access(outputPath);
+  } catch (e) {
+    return;
+  }
+
+  for (const e of await fs.readdir('fixtures/001/output')) {
+    await fs.unlink(`fixtures/001/output/${e}`);
+  }
+  await fs.rmdir('fixtures/001/output');
+}

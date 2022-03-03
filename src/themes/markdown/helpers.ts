d1935a878ab829ced759610c7b9449cef30148d4
Francisco Giordano <frangio.1@gmail.com>
2022-03-02 22:48:45 -0300

    refactor heading helpers

--- src/themes/markdown/helpers.ts
+++ src/themes/markdown/helpers.ts
@@ -2,9 +2,6 @@ import { HelperOptions, Utils } from 'handlebars';
 import { TypeName } from 'solidity-ast';
 import { DocItemWithContext } from '../../site';
 
-type HLevel = { hlevel?: number };
-type DocItemWithHLevel = DocItemWithContext & HLevel;
-
 /**
  * Returns a Markdown heading marker. An optional number increases the heading level.
  *
@@ -12,10 +9,11 @@ type DocItemWithHLevel = DocItemWithContext & HLevel;
  *    {{h}} {{name}}         # Name
  *    {{h 2}} {{name}}       ## Name
  */
-export function h(this: DocItemWithHLevel, hsublevel: number | HelperOptions) {
-  ({ hsublevel } = getHSublevel(hsublevel));
-  hsublevel = typeof hsublevel === 'number' ? Math.max(1, hsublevel) : 1;
-  return new Array(getHLevel(this) + hsublevel - 1).fill('#').join('');
+export function h(opts: HelperOptions): string;
+export function h(hsublevel: number, opts: HelperOptions): string;
+export function h(hsublevel: number | HelperOptions, opts?: HelperOptions) {
+  const { hlevel } = getHLevel(hsublevel, opts);
+  return new Array(hlevel).fill('#').join('');
 };
 
 /**
@@ -25,32 +23,29 @@ export function h(this: DocItemWithHLevel, hsublevel: number | HelperOptions) {
  *    {{>partial-with-headings}}
  *    {{/hsection}}
  */
-export function hsection(this: DocItemWithHLevel, opts: HelperOptions): string;
-export function hsection(this: DocItemWithHLevel, hsublevel: number, opts: HelperOptions): string;
-export function hsection(this: DocItemWithHLevel, hsublevel: number | HelperOptions, opts?: HelperOptions) {
-  ({ hsublevel, opts } = getHSublevel(hsublevel, opts));
-  const hlevel = getHLevel(this) + hsublevel;
-  const ctx = Utils.extend({}, this, { hlevel });
-  return opts.fn(ctx, opts);
+export function hsection(opts: HelperOptions): string;
+export function hsection(hsublevel: number, opts: HelperOptions): string;
+export function hsection(this: unknown, hsublevel: number | HelperOptions, opts?: HelperOptions) {
+  let hlevel;
+  ({ hlevel, opts } = getHLevel(hsublevel, opts));
+  opts.data = Utils.createFrame(opts.data);
+  opts.data.hlevel = hlevel;
+  return opts.fn(this as unknown, opts);
 }
 
 /**
  * Helper for dealing with the optional hsublevel argument.
  */
-function getHSublevel(hsublevel: number | HelperOptions): { hsublevel: number };
-function getHSublevel(hsublevel: number | HelperOptions, opts?: HelperOptions): { hsublevel: number, opts: HelperOptions };
-function getHSublevel(hsublevel: number | HelperOptions, opts?: HelperOptions) {
+function getHLevel(hsublevel: number | HelperOptions, opts?: HelperOptions) {
   if (typeof hsublevel === 'number') {
     opts = opts!;
-    return { hsublevel: Math.max(1, hsublevel), opts };
+    hsublevel = Math.max(1, hsublevel);
   } else {
     opts = hsublevel;
-    return { hsublevel: 1, opts };
+    hsublevel = 1;
   }
-}
-
-function getHLevel(ctx: HLevel): number {
-  return ctx.hlevel ?? 1;
+  const contextHLevel: number = opts.data?.hlevel ?? 0;
+  return { opts, hlevel: contextHLevel + hsublevel };
 }
 
 export function trim(text: string) {

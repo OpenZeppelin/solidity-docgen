54abfc4ae9a9f17c219463a0ac5896e29065ee46
Francisco Giordano <frangio.1@gmail.com>
2022-06-24 12:50:12 -0300

    fix type error

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -1,4 +1,4 @@
-import _test, { TestInterface } from 'ava';
+import _test, { TestFn } from 'ava';
 import hre from 'hardhat';
 import { BuildInfo } from 'hardhat/types';
 import { promises as fs } from 'fs';
@@ -7,7 +7,7 @@ interface Context {
   build: BuildInfo[];
 }
 
-const test = _test as TestInterface<Context>;
+const test = _test as TestFn<Context>;
 
 test.before('reading build info', async t => {
   t.context.build = await Promise.all(

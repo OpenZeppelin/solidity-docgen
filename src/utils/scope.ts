a1215b65d38483296b5c98f86f8e8fff529edcd3
Francisco Giordano <frangio.1@gmail.com>
2022-08-30 01:12:48 -0300

    implement full support for circular dependencies

--- src/utils/scope.ts
+++ src/utils/scope.ts
@@ -1,51 +1,41 @@
 import { ContractDefinition, SourceUnit } from "solidity-ast";
 import { findAll } from "solidity-ast/utils";
 import { DocItemWithContext } from "../site";
+import { mapValues } from './map-values';
 
 export function getContractsInScope(item: DocItemWithContext) {
-  const cache = new WeakMap<SourceUnit, Record<string, ContractDefinition>>();
-  return run(item.__item_context.file);
+  const cache = new WeakMap<SourceUnit, Record<string, () => ContractDefinition>>();
+  return mapValues(run(item.__item_context.file), fn => fn());
 
   function run(
     file: SourceUnit,
-    stack = new Set<SourceUnit>(),
     aliasedImport = false,
-  ): Record<string, ContractDefinition> {
-    if (stack.has(file)) {
-      if (aliasedImport) {
-        throw new Error('Circular dependency detected: aliased imports not supported');
-      } else {
-        return {};
-      }
-    }
-
+  ): Record<string, () => ContractDefinition> {
     if (cache.has(file)) {
       return cache.get(file)!;
     }
 
-    stack.add(file);
+    const scope: Record<string, () => ContractDefinition> = {};
 
-    const scope: Record<string, ContractDefinition> = {};
+    cache.set(file, scope);
 
     for (const c of findAll('ContractDefinition', file)) {
-      scope[c.name] = c;
+      scope[c.name] = () => c;
     }
 
     for (const i of findAll('ImportDirective', file)) {
       const importedFile = item.__item_context.build.deref('SourceUnit', i.sourceUnit);
-      const importedScope = run(importedFile, stack, aliasedImport || i.symbolAliases.length > 0);
+      const importedScope = run(importedFile, aliasedImport || i.symbolAliases.length > 0);
       if (i.symbolAliases.length === 0) {
         Object.assign(scope, importedScope);
       } else {
         for (const a of i.symbolAliases) {
-          scope[a.local ?? a.foreign.name] = importedScope[a.foreign.name]!;
+          // Delayed function call supports circular dependencies
+          scope[a.local ?? a.foreign.name] = importedScope[a.foreign.name] ?? (() => importedScope[a.foreign.name]!());
         }
       }
     };
 
-    stack.delete(file);
-
-    cache.set(file, scope);
     return scope;
   }
 }
